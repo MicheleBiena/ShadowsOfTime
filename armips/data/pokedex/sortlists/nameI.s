b2e7b8fc210ff20e7ce109a2be9164f5e389fda3
TrainerFromHoenn <michelebiena@gmail.com>
2024-11-18 17:33:33 +0100

    First

---
+++ armips/data/pokedex/sortlists/nameI.s
@@ -0,0 +1,13 @@
+.nds
+.thumb
+
+.create "a214/026.bin", 0x0
+
+.halfword SPECIES_IGGLYBUFF
+.halfword SPECIES_IGNISOAR
+.halfword SPECIES_ILLUMISE
+.halfword SPECIES_INFERNAPE
+.halfword SPECIES_IVYSAUR
+
+
+.close
---
+++ data/battle_scripts/moves/move_script_0803_METEOR_BEAM.s
@@ -0,0 +1,8 @@
+.include "asm/include/battle_commands.inc"
+
+.data
+
+_000:
+    // {0} is overflowing with space power!
+    BufferMessage 1477, TAG_NICKNAME, BATTLER_CATEGORY_ATTACKER
+    GoToEffectScript  
